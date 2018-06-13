79d4368fcb612ea1990d4a94d6fed1dbdbd0fb04
alysonNBS <alyson_nbs2.0@hotmail.com>
2018-06-13 11:03:43 -0300

    grupos sao printados ordenados-completando commit

--- funcoesFaseGrupo.h
+++ funcoesFaseGrupo.h
@@ -7,15 +7,15 @@ int saldo(struct selecao s);
 int gp(struct selecao s);
 int gc(struct selecao s);
 
+int classificacao(const void *s1, const void *s2);
+int padrao(const void *s1, const void *s2);
 
 
-int pontos(struct selecao s)
-{
+int pontos(struct selecao s) {
     int i, p;
 
     for(p=i=0; i<3; i++)
-        if(s.jogo[i]->placar[0] != -1)
-        {
+        if(s.jogo[i]->placar[0] != -1) {
             if(empate(*s.jogo[i]))
                 p+=1;
             else if(string_comp(s.nome, vencedor(*s.jogo[i])->nome) == 0)
@@ -27,8 +27,7 @@ int pontos(struct selecao s)
 
 
 
-int jogos(struct selecao s)
-{
+int jogos(struct selecao s) {
     int i, j;
 
     for(j=i=0; i<3; i++)
@@ -40,13 +39,11 @@ int jogos(struct selecao s)
 
 
 
-int vitorias(struct selecao s)
-{
+int vitorias(struct selecao s) {
     int i, v;
 
     for(v=i=0; i<3; i++)
-        if(s.jogo[i]->placar[0] != -1)
-        {
+        if(s.jogo[i]->placar[0] != -1) {
             Tselecao *vencedora = vencedor(*s.jogo[i]);
 
             if(vencedora != NULL && string_comp(s.nome, vencedora->nome) == 0)
@@ -58,8 +55,7 @@ int vitorias(struct selecao s)
 
 
 
-int empates(struct selecao s)
-{
+int empates(struct selecao s) {
     int i, e;
 
     for(e=i=0; i<3; i++)
@@ -72,8 +68,7 @@ int empates(struct selecao s)
 
 
 
-int derrotas(struct selecao s)
-{
+int derrotas(struct selecao s) {
     int i, d;
 
     for(d=i=0; i<3; i++)
@@ -86,20 +81,17 @@ int derrotas(struct selecao s)
 
 
 
-int saldo(struct selecao s)
-{
+int saldo(struct selecao s) {
     return gp(s) - gc(s);
 }
 
 
 
-int gp(struct selecao s)
-{
+int gp(struct selecao s) {
     int i, gols;
 
     for(gols=i=0; i<3; i++)
-        if(s.jogo[i]->placar[0] != -1)
-        {
+        if(s.jogo[i]->placar[0] != -1) {
             if(string_comp(s.nome, s.jogo[i]->pais[0]->nome) == 0)
                 gols += s.jogo[i]->placar[0];
             else
@@ -111,13 +103,11 @@ int gp(struct selecao s)
 
 
 
-int gc(struct selecao s)
-{
+int gc(struct selecao s) {
     int i, gols;
 
     for(gols=i=0; i<3; i++)
-        if(s.jogo[i]->placar[0] != -1)
-        {
+        if(s.jogo[i]->placar[0] != -1) {
             if(string_comp(s.nome, s.jogo[i]->pais[0]->nome) == 0)
                 gols += s.jogo[i]->placar[1];
             else
